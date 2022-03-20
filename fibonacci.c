2afc21546ea725ab5f7c81833c5cb9b528155a08
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-03-20 20:44:49 -0300

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main()
+/*el lenguaje C corresponde al paradigma de la programacón Estructurada o modular  */
 {
 int n, a=0,b=1,temp,i;
 printf("ingrese hasta que Nro hacer Fibonacci: ");
