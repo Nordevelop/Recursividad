20dd2ef700216db65193360e6f2713c704fabfef
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-03-20 15:34:46 -0300

    Create triangle of squares.java

---
+++ triangle of squares.java
@@ -0,0 +1,40 @@
+package triangulo;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n;
+
+	public static void main(String[] args) throws IOException {
+		/*Triangulo de cuadrados
+		 * Author Nordevelop
+		 * Recursividad
+		 */
+		System.out.print("ingrese un numero: ");
+		n= getInt();
+		int theAnswer = triangulo(n);
+		System.out.println("Triangulo="+theAnswer);
+		} // end main()
+	public static int triangulo(int n)
+	{
+	if(n==1)
+	return 1;
+	else
+	return( n + triangulo(n-1));
+	}
+	public static String getString() throws IOException
+	{
+	InputStreamReader isr = new InputStreamReader(System.in);
+	BufferedReader br = new BufferedReader(isr);
+	String s = br.readLine();
+	return s;
+	}
+	
+	public static int getInt() throws IOException
+	{
+	String s = getString();
+	return Integer.parseInt(s);
+	}
+	
+		
+}
