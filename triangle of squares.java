9bdbf4d4d35fa32e192dcbf1253695eaaf3efc59
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-03-20 16:47:17 -0300

    Update triangle of squares.java

--- triangle of squares.java
+++ triangle of squares.java
@@ -6,35 +6,35 @@ public class Main {
 	static int n;
 
 	public static void main(String[] args) throws IOException {
-		/*Triangulo de cuadrados
-		 * Author Nordevelop
-		 * Recursividad
-		 */
-		System.out.print("ingrese un numero: ");
-		n= getInt();
-		int theAnswer = triangulo(n);
-		System.out.println("Triangulo="+theAnswer);
-		} // end main()
-	public static int triangulo(int n)
-	{
-	if(n==1)
-	return 1;
+	   /*Triangulo de cuadrados
+	    * Author Nordevelop
+	    * Recursividad
+	    */
+	   System.out.print("ingrese un numero: ");
+	      n= getInt();
+	   int theAnswer = triangulo(n);
+	   System.out.println("Triangulo="+theAnswer);
+	} // end main()
+	public static int triangulo(int n){
+	
+        if(n==1)
+	   return 1;
 	else
-	return( n + triangulo(n-1));
-	}
-	public static String getString() throws IOException
-	{
+	   return( n + triangulo(n-1));
+	} //fin metodo trinagulo
+	public static String getString() throws IOException{
+		
 	InputStreamReader isr = new InputStreamReader(System.in);
 	BufferedReader br = new BufferedReader(isr);
 	String s = br.readLine();
 	return s;
-	}
+	} //fin metodo getString
 	
-	public static int getInt() throws IOException
-	{
+	public static int getInt() throws IOException{
+		
 	String s = getString();
 	return Integer.parseInt(s);
-	}
+	}//fin metodo getInt
 	
 		
-}
+}fin Main
